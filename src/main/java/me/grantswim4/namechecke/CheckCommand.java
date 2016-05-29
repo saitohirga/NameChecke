e6a511bfe68939795880dde966a17651da553a91
GRANTSWIM4 <grant@hqm.me>
2016-05-29 15:08:22 -0400

    Todo add new JSON Paser

--- src/main/java/me/grantswim4/namechecke/CheckCommand.java
+++ src/main/java/me/grantswim4/namechecke/CheckCommand.java
@@ -6,7 +6,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.json.JSONException;
 
 import java.io.IOException;
 
--- src/main/java/me/grantswim4/namechecke/mojangapi.java
+++ src/main/java/me/grantswim4/namechecke/mojangapi.java
@@ -1,35 +1,10 @@
 package me.grantswim4.namechecke;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.Charset;
-
 /**
  * Created by Grant on 5/21/2016.
  */
 public class mojangapi {
 
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
-        }
-    }
+    // todo add a system to prase Json and get Json form Mojang with JsonSimple.
 }
