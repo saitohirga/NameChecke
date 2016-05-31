3554c836c31bc091cfdc948582f66daa49cdf982
GRANTSWIM4 <grant@hqm.me>
2016-05-30 21:44:47 -0400

    Kind of works now. A little more ways.

--- src/main/java/me/grantswim4/namechecke/CheckCommand.java
+++ src/main/java/me/grantswim4/namechecke/CheckCommand.java
@@ -1,14 +1,13 @@
 package me.grantswim4.namechecke;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.json.JSONException;
 
 import java.io.IOException;
-
 
 public class CheckCommand implements CommandExecutor {
 
