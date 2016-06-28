c88f971a11ee3a135e1c99d49dea1f9b4293ea52
Grant <GRANTSWIM4@users.noreply.github.com>
2016-06-28 10:02:33 -0400

    The GSON update Pt 1.

--- src/main/java/me/grantswim4/namechecke/CheckCommand.java
+++ src/main/java/me/grantswim4/namechecke/CheckCommand.java
@@ -5,44 +5,50 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.json.JSONException;
+
+import com.google.gson.JsonIOException;
 
 import java.io.IOException;
 
 public class CheckCommand implements CommandExecutor {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!sender.hasPermission("NameChecker.Check")) {
-            sender.sendMessage("Sorry, you don't have access to this. Permission: NameChecker.Check");
-        } else {
+	String uuidWOH;
 
-            Player ply = Bukkit.getPlayer(args[0]);
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!sender.hasPermission("NameChecker.Check")) {
+			sender.sendMessage("Sorry, you don't have access to this. Permission: NameChecker.Check");
+		} else {
 
-            if (ply != null) {
-                sender.sendMessage("Target: " + ply.getDisplayName() + "(" + ply.getUniqueId() + ")");
-                sender.sendMessage("Website: ");
-                sender.sendMessage("https://api.mojang.com/user/profiles/" + ply.getUniqueId() + "/names");
-                  
-                sender.sendMessage("Attempting InGame Results...");
-                try {
-                    ply.getUniqueId.replaceAll("[\\s\\-()]", "");
-                    String tar = mojangapi.readJsonFromUrl("https://api.mojang.com/user/profiles/" + ply.getUniqueId() + "/names").toString();
-                    for (String line : "},{".split(tar)) {
-                        sender.sendMessage(line);
-                    }
-                } catch (JSONException e) {
-                    sender.sendMessage("Failed to grab ingame results!");
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    sender.sendMessage("Failed to grab ingame results!");
-                    e.printStackTrace();
-                }
+			Player ply = Bukkit.getPlayer(args[0]);
 
-            }
+			uuidWOH = ply.getUniqueId().toString();
 
-        }
-        return false;
-    }
+			if (ply != null) {
+				sender.sendMessage("Target: " + ply.getDisplayName() + "(" + ply.getUniqueId() + ")");
+				sender.sendMessage("Website: ");
+				sender.sendMessage("https://api.mojang.com/user/profiles/" + ply.getUniqueId() + "/names");
+
+				sender.sendMessage("Attempting InGame Results...");
+				try {
+
+					uuidWOH.replaceAll("[\\s\\-()]", "");
+					String tar = mojangapi.readJsonFromUrl("https://api.mojang.com/user/profiles/" + uuidWOH + "/names").toString();
+					for (String line : "},{".split(tar)) {
+						sender.sendMessage(line);
+					}
+				} catch (JsonIOException e) {
+					sender.sendMessage("Failed to grab ingame results!");
+					e.printStackTrace();
+				} catch (IOException e) {
+					sender.sendMessage("Failed to grab ingame results!");
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+		return false;
+	}
 
 }
